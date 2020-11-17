117af202898675cb7d36ec56fe8554960ff09fb5
vlobellova <vlobellova@gmail.com>
2020-11-17 14:03:56 +0100

    Delete angle2point.m

--- angle2point.m
+++
@@ -1,7 +0,0 @@
-function [x, y] = angle2point(x, y, len, angle)
-%creates point2[x,y] in length and angle from point1
-
-x = x - (len * cos(pi * angle /180));
-y = y + (len * sin(pi * angle /180));
-
-end
